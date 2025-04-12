44f6fb585d4e2c833f87893b14e46df9f2c0263c
Matheus Oliveira da Silva <matheus.6148@gmail.com>
2025-04-12 20:32:47 -0300

    refactor: remove placeholder comment in onSubmit method

---
+++ src/app/auth/signin/sigin.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sigin',
+  imports: [],
+  templateUrl: './signin.component.html',
+  styleUrl: './signin.component.scss'
+})
+export class SiginComponent {
+
+}
