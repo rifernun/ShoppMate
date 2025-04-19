79e8793918f88b586c385c72545260fabce9f4ca
Matheus Oliveira da Silva <matheus.6148@gmail.com>
2025-04-19 19:07:21 -0300

    feat: refactor login and signin components to use Angular Material for improved UI and accessibility; remove unused menu component

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   standalone: true,
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
-  imports: [ReactiveFormsModule] // importa o módulo reativo aqui
+  imports: [
+    ReactiveFormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatCardModule
+  ]
 })
 export class LoginComponent {
   loginForm: FormGroup;
--- src/app/auth/signin/signin.component.ts
+++ src/app/auth/signin/signin.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   standalone: true,
   selector: 'app-signin',
   templateUrl: './signin.component.html',
   styleUrl: './signin.component.scss',
-  imports: [ReactiveFormsModule]
+  imports: [
+    ReactiveFormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatCardModule
+  ]
 })
 export class SigninComponent {
   signinForm: FormGroup;
